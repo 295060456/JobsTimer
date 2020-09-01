564bba8e64634cc32873d52f3370d15a9efaac88
Jobs <>
2020-09-01 16:20:51 +0700

    no message

--- Timer/业务/ViewController.m
+++ Timer/业务/ViewController.m
@@ -8,10 +8,12 @@
 
 #import "ViewController.h"
 #import "TimerManager.h"
+#import "UIButton+CountDownBtn.h"
 
 @interface ViewController ()
 
 @property(nonatomic,strong)NSTimerManager *nsTimerManager;
+@property(nonatomic,strong)UIButton *countDownBtn;
 
 @end
 
@@ -19,10 +21,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor redColor];
+//    self.view.backgroundColor = [UIColor redColor];
+    
+    self.countDownBtn.alpha = 1;
+    
     //创建方式——1
-    [NSTimerManager nsTimeStart:self.nsTimerManager.nsTimer
-                    withRunLoop:nil];
+//    [NSTimerManager nsTimeStart:self.nsTimerManager.nsTimer
+//                    withRunLoop:nil];
     //创建方式——2
 //    [self.nsTimerManager nsTimeStartSysAutoInRunLoop];
 }
@@ -34,6 +39,21 @@
             NSLog(@"你好");
         }];
     }return _nsTimerManager;
+}
+
+-(UIButton *)countDownBtn{
+    if (!_countDownBtn) {
+        _countDownBtn = UIButton.new;
+        _countDownBtn.titleRuningStr = @"重新发送";
+        _countDownBtn.titleBeginStr = @"重新发送";
+        _countDownBtn.titleColor = [UIColor blueColor];
+        _countDownBtn.bgCountDownColor = [UIColor redColor];;
+        _countDownBtn.bgEndColor = [UIColor clearColor];;
+        _countDownBtn.layerCornerRadius = 6;
+        [_countDownBtn timeFailBeginFrom:60];
+        [self.view addSubview:_countDownBtn];
+        _countDownBtn.frame = CGRectMake(100, 100, 100, 100);
+    }return _countDownBtn;
 }
 
 @end
