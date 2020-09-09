e9d9201bbf4fb3856e40e982247714ed62e85677
Jobs <>
2020-09-09 18:54:16 +0700

    no message

--- Timer/业务/ViewController.m
+++ Timer/业务/ViewController.m
@@ -37,10 +37,10 @@
 }
 
 -(void)makeTimer{
-    //创建方式——1
+    //启动方式——1
     //    [NSTimerManager nsTimeStart:self.nsTimerManager.nsTimer
     //                    withRunLoop:nil];
-    //创建方式——2
+    //启动方式——2
     [self.nsTimerManager nsTimeStartSysAutoInRunLoop];
 }
 
