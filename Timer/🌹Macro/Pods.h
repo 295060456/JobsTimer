ac827f3eee64ba2dd711f9cf4a00791376eeccd0
Jobs <lg295060456@gmail.com>
2020-12-08 19:10:05 +0700

    no message

--- Timer/🌹Macro/Pods.h
+++ Timer/🌹Macro/Pods.h
@@ -9,9 +9,28 @@
 #ifndef Pods_h
 #define Pods_h
 
+#if __has_include(<Masonry/Masonry.h>)
 #import <Masonry/Masonry.h>
-#import <ReactiveObjC.h>
+#else
+#import "Masonry.h"
+#endif
+
+#if __has_include(<ReactiveObjC/ReactiveObjC.h>)
+#import <ReactiveObjC/ReactiveObjC.h>
+#else
+#import "ReactiveObjC.h"
+#endif
+
+#if __has_include(<MJExtension/MJExtension.h>)
+#import <MJExtension/MJExtension.h>
+#else
 #import "MJExtension.h"
+#endif
+
+#if __has_include(<MJRefresh/MJRefresh.h>)
+#import <MJRefresh/MJRefresh.h>
+#else
 #import "MJRefresh.h"
+#endif
 
 #endif /* Pods_h */
