f2f94ec795259f1d7fd47042fb5b0702235d9dd4
Jobs <lg295060456@gmail.com>
2021-02-28 14:35:56 +0700

    no message

--- Timer/🌹Macro/其他杂项/Other.h
+++ Timer/🌹Macro/其他杂项/Other.h
@@ -28,8 +28,8 @@ static inline UIWindow * getMainWindow(){
 #define SCREEN_WIDTH    Device_Width
 #define SCREEN_HEIGHT   Device_Height
 //输入原型图上的宽和高，对外输出App对应的移动设备的真实宽高
-#define KWidth(width) (SCREEN_WIDTH / 375) * width //375 对应原型图的宽
-#define KHeight(height) (SCREEN_HEIGHT / 743) * height //743 对应原型图的高
+#define KWidth(width) (SCREEN_WIDTH / 375) * (width) //375 对应原型图的宽
+#define KHeight(height) (SCREEN_HEIGHT / 743) * (height) //743 对应原型图的高
 
 // http://ww1.fuckingclangwarnings.com/
 //#pragma clang diagnostic push
